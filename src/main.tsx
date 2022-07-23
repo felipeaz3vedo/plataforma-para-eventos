938a707a87e4d32b0797ecf7525d1d550fc037bd
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-07-23 00:03:56 -0300

    boilerplate da aplicação

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import "./styles/global.css";
+
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://api-sa-east-1.hygraph.com/v2/cl5w928g90uz501uk7mttddol/master",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
-)
+);
