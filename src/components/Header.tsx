3510f69a8133f48d50b15474f39dfed8ae5e24b6
felipeaz3vedo <felipedeazevedopessoa@gmail.com>
2022-07-26 05:50:52 -0300

    Configuração das rotas usando react-router-dom e configuração dos componentes para mudarem dinâmicamente de acordo com as informações dos videos dispostas no cms

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,6 +4,9 @@ export function Header() {
   return (
     <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
       <Logo />
+      <div className="w-[348px]">
+
+      </div>
     </header>
-  )
+  );
 }
